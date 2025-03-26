b460624612a5c7ac685847491406f01cf1870f6e
naeemullah <naeem.ullah@veroke.com>
2025-03-26 21:51:28 +0500

    add rate limiter to APIs

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import "dotenv/config"
 const app = express();
 import fileUpload from 'express-fileupload';
+import helmet from 'helmet';
+import cors from 'cors'
+import { limiter } from './config/ratelimiter.js';
 const PORT = process.env.PORT || 5000;
 
 // Middlewares
@@ -9,6 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.static("public"));
 app.use(fileUpload());
+app.use(helmet());
+app.use(cors())
+app.use(limiter)
 
 
 app.get('/',(req,res)=>{
