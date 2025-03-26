e9a0e8c9765b8590b170b91ebd35e65f3c1c5040
naeemullah <naeem.ullah@veroke.com>
2025-03-26 21:20:53 +0500

    update and delete news api done

--- controllers/NewsController.js
+++ controllers/NewsController.js
@@ -1,6 +1,6 @@
 import vine, { errors } from "@vinejs/vine";
 import { newsSchema } from "../validations/newsValidation.js";
-import { generateRandomNum, imageValidator } from "../utils/helper.js";
+import { generateRandomNum, imageValidator, removeImage, uploadImage } from "../utils/helper.js";
 import prisma from "../DB/db.config.js";
 import NewsApiTransform from "../transform/newsApiTransform.js";
 
@@ -68,15 +68,7 @@ class NewsController {
             }
 
             // Image upload
-            const imageExt = image?.name.split(".");
-            const imageName = generateRandomNum() + "." + imageExt[1];
-            const uploadPath = process.cwd() + "/public/images/" + imageName;
-
-            image.mv(uploadPath, (err) => {
-                if (err) {
-                    throw err;
-                }
-            })
+            const imageName = uploadImage(image);
             payload.image = imageName;
             payload.user_id = user.id;
             const news = await prisma.news.create({
@@ -97,33 +89,110 @@ class NewsController {
 
     }
     static async show(req, res) {
-       try {
-        const { id } = req.params;
-        const news = await prisma.news.findUnique({
-            where: {
-                id: Number(id)
-            },
-            include: {
-                user: {
-                    select: {
-                        id: true,
-                        name: true,
-                        profile: true
+        try {
+            const { id } = req.params;
+            const news = await prisma.news.findUnique({
+                where: {
+                    id: Number(id)
+                },
+                include: {
+                    user: {
+                        select: {
+                            id: true,
+                            name: true,
+                            profile: true
+                        }
                     }
                 }
-            }
-        })
-        const transFormNews = news ? NewsApiTransform.transform(news) : null;
-        return res.json({ status: 200, news: transFormNews })
-       } catch (error) {
-            return res.status(500).json({message:"Something went wrong please try again"})
-       }
+            })
+            const transFormNews = news ? NewsApiTransform.transform(news) : null;
+            return res.json({ status: 200, news: transFormNews })
+        } catch (error) {
+            return res.status(500).json({ message: "Something went wrong please try again" })
+        }
     }
     static async update(req, res) {
+        try {
+            const { id } = req.params;
+            const user = req.user;
+            const body = req.body;
+            const news = await prisma.news.findUnique({
+                where: {
+                    id: Number(id)
+                }
+            })
+            if (user.id !== news.user_id) {
+                return res.status(400).json({ message: "UnAuthorized" })
+            }
+
+            const validator = vine.compile(newsSchema);
+            const payload = await validator.validate(body);
+            const image = req?.files?.image;
+            if (image) {
+                const message = imageValidator(image?.size, image?.mimetype);
+                if (message !== null) {
+                    return res.status(400).json({
+                        errors: {
+                            image: message
+                        }
+                    })
+                }
+
+                // Upload new image
+
+                const  imageName = uploadImage(image);
+                payload.image = imageName;
+                // Delete old image
+                removeImage(news.image);
+
+            }
+
+
+            await prisma.news.update({
+                data: payload,
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            return res.status(200).json({message:"News Updated successfully"});
+        } catch (error) {
+            console.log("Error is ", error);
+            if (error instanceof errors.E_VALIDATION_ERROR) {
+                // console.log(error.messages);
+                return res.status(400).json({ errors: error.messages })
+            } else {
+                return res.status(500).json({ state: 500, message: "Something went wrong, Please try again!" })
+            }
+        }
 
     }
     static async destroy(req, res) {
+       try {
+        const {id }= req.params;
+        const user = req.user;
+        const news = await prisma.news.findUnique({
+            where:{
+                id:Number(id)
+            }
+        })
+        if(user.id !== news?.user_id){
+            return res.status(401).json({message:"UnAuthorized"});
+        }
 
+        // Delete image from server
+        removeImage(news.image);
+        await prisma.news.delete({
+            where:{
+                id:Number(id)
+            }
+        })
+
+        return res.status(200).json({message:"News has been deleted"});
+       } catch (error) {
+        return res.status(500).json({ state: 500, message: "Something went wrong, Please try again!" })
+
+       }
     }
 
 }
--- utils/helper.js
+++ utils/helper.js
@@ -1,10 +1,11 @@
 import { supportedMines } from "../config/filesystem.js"
-import {v4 as uuid} from "uuid"
-export const imageValidator = (size,mime)=>{
-    if(bytesToMB(size)>2){
+import fs from 'fs';
+import { v4 as uuid } from "uuid"
+export const imageValidator = (size, mime) => {
+    if (bytesToMB(size) > 2) {
         return "Image size must be less than 2 MB"
     }
-    else if(!supportedMines.includes(mime)){
+    else if (!supportedMines.includes(mime)) {
         return "Image must be type of png,jpg,jpeg,svg,webp,gif";
     }
 
@@ -20,6 +21,31 @@ export const generateRandomNum = () => {
 }
 
 
-export const getImageUrl = (imgName)=>{
+export const getImageUrl = (imgName) => {
     return `${process.env.APP_URL}/images/${imgName}`
+}
+
+export const removeImage = (imageName) => {
+    console.log("image name is ", imageName);
+    const path = process.cwd() + "/public/images/" + imageName;
+    if (fs.existsSync(path)) {
+        fs.unlinkSync(path);
+    }
+}
+
+
+export const uploadImage = (image) => {
+    // Image upload
+    const imageExt = image?.name.split(".");
+    const imageName = generateRandomNum() + "." + imageExt[1];
+    const uploadPath = process.cwd() + "/public/images/" + imageName;
+
+    image.mv(uploadPath, (err) => {
+        if (err) {
+            throw err;
+        }
+    })
+
+    return imageName;
+
 }
